013e0c28e75b03f85a3f5f1f254b82d668b33370
Victor Sanchez <vickoss9004@gmail.com>
2025-03-29 14:22:19 -0600

    Otro intento de los inputs
    Ayuda por favor

---
+++ Assets/Scripts/TickManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class TickManager : MonoBehaviour
+{
+    public float ticksPerSecond;
+    private float tickInterval;
+    public int tickCount;
+
+    public event Action<int> OnTick;
+
+    void Start()
+    {
+        UpdateTickInterval();
+        StartCoroutine(TickLoop());
+    }
+
+    void UpdateTickInterval()
+    {
+        tickInterval = 1f / ticksPerSecond;
+    }
+
+    IEnumerator TickLoop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(tickInterval);
+            tickCount++;
+            //print($"Tick {tickCount} - Intervalo: {tickInterval} segundos");
+
+            OnTick?.Invoke(tickCount); 
+        }
+    }
+
+    public void SetTicksPerSecond(float newTicksPerSecond)
+    {
+        ticksPerSecond = newTicksPerSecond;
+        UpdateTickInterval();
+    }
+}
