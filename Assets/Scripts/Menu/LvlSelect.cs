7b27690df2d8ebb656b008714ed4781799766f02
Victor Sanchez <vickoss9004@gmail.com>
2025-06-02 01:15:16 -0600

    Agregando 2 niveles
    Nivel con la musica que hicieron los musicos y otro nivel con una cancion que me gusta >:D

--- Assets/Scripts/Menu/LvlSelect.cs
+++ Assets/Scripts/Menu/LvlSelect.cs
@@ -1,12 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class LvlSelect : MonoBehaviour
 {
+    public AudioSource audioSource; 
+    public AudioClip sonidoCarga;   
+    public float delayAntesDeCargar;
+    public Image pantallaNegra;
+    public float duracionFade;
+    public GameObject musicManager;
+    public GameObject canvasBack;
+
+
     public void CargarEscena(string nombreEscena)
     {
+        StartCoroutine(TransicionEscena(nombreEscena));
+    }
+
+    private IEnumerator TransicionEscena(string nombreEscena)
+    {
+        if (musicManager != null)
+        {
+            musicManager.SetActive(false);
+            canvasBack.SetActive(false);
+        }
+
+        if (audioSource != null && sonidoCarga != null)
+        {
+            audioSource.PlayOneShot(sonidoCarga);
+        }
+
+        if (pantallaNegra != null)
+        {
+            Color color = pantallaNegra.color;
+            for (float t = 0; t < duracionFade; t += Time.deltaTime)
+            {
+                float alpha = Mathf.Lerp(0f, 1f, t / duracionFade);
+                pantallaNegra.color = new Color(color.r, color.g, color.b, alpha);
+                yield return null;
+            }
+            pantallaNegra.color = new Color(color.r, color.g, color.b, 1f);
+        }
+
+        yield return new WaitForSeconds(delayAntesDeCargar);
+
         SceneManager.LoadScene(nombreEscena);
     }
 }
